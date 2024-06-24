86b683b245979403d79ea6e58290c137e87117c7
flavioandrade91 <flavio.andrade91@outlook.com.br>
2024-06-24 01:18:21 -0300

    apiGraph

---
+++ FuelManagerGraphQL/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FuelManagerGraphQL.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options
+            ) : base(options) {}
+
+        public DbSet<Veiculo> Veiculos { get; set;}
+        public DbSet<Consumo> Consumos { get; set;}
+    }
+}
